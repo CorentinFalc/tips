5af232c0da95f5b5cca45c8492ec3adc5af38c42
Corentin Falcone <akwalith@gmail.com>
2018-11-06 17:51:01 +0100

    Adding powerful st_difference

--- sql/tips.sql
+++ sql/tips.sql
@@ -7,3 +7,15 @@ FROM pg_catalog.pg_statio_all_tables as st
   and c.table_schema=st.schemaname and c.table_name=st.relname)
 
    order by table_name;
+
+
+
+	--Difference efficace
+Create materialized view sandbox.vm_mos_2015_rest_v2t as
+with tmp as (
+  select b.gid, st_union(a.geom) as geom
+  from sandbox.vm_mos_2015_parcellaire b 
+  join sandbox.vm_geo_parc_subd_for_comp a on st_intersects(a.geom, b.new_geom)
+  group by b.gid
+) select b.gid, st_difference(b.new_geom,coalesce(t.geom, 'GEOMETRYCOLLECTION EMPTY'::geometry)) as newgeom
+from sandbox.vm_mos_2015_parcellaire b left join tmp t on b.gid = t.gid;
